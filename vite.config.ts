73f95a6fef0c335f203698d19d94b47ab7007942
zhangjinjie <zhangjinjie@luojilab.com>
2022-06-12 11:17:56 +0800

    feat: add commitlint; add vite-plugin-eslint;

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import eslint from 'vite-plugin-eslint'
 
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    eslint({
+      cache: false
+    })
+  ]
 })
